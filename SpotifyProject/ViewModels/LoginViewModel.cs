a8430e67407b082693e0f62460a4abba8b59bad6
Selim Berk Tetik <selimberktetik@icloud.com>
2024-01-09 20:32:24 +0300

    with error CS0116

--- SpotifyProject/ViewModels/LoginViewModel.cs
+++ SpotifyProject/ViewModels/LoginViewModel.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SpotifyProject.ViewModels
 {
-    public class LoginViewModel
+    public partial class LoginViewModel : ViewModel
     {
+    }
+
+    public LoginViewModel() { }
+
+
+    [ObservableProperty]
+    private bool showLogin;
+
+
+    [RelayCommand]
+private void OpenLogin()
+    {
+        ShowLogin = true;
     }
 }
